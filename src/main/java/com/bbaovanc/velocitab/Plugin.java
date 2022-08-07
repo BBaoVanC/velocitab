26ec23c042ca8fc13bdd48b6fdc29846b338355a
BBaoVanC <bbaovanc@bbaovanc.com>
2022-08-07 18:47:21 -0500

    Almost done, GroupDataRecalculateEvent doesn't work

--- src/main/java/com/bbaovanc/velocitab/Plugin.java
+++ src/main/java/com/bbaovanc/velocitab/Plugin.java
@@ -1,24 +1,26 @@
 package com.bbaovanc.velocitab;
 
 import com.google.inject.Inject;
-import com.velocitypowered.api.event.EventManager;
 import com.velocitypowered.api.event.Subscribe;
-import com.velocitypowered.api.event.player.PlayerChatEvent;
-import com.velocitypowered.api.event.player.ServerConnectedEvent;
 import com.velocitypowered.api.event.player.ServerPostConnectEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.ServerConnection;
 import com.velocitypowered.api.proxy.player.TabList;
-import com.velocitypowered.api.proxy.player.TabListEntry;
-import com.velocitypowered.api.util.GameProfile;
-import net.kyori.adventure.text.Component;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.event.EventBus;
+import net.luckperms.api.event.group.GroupDataRecalculateEvent;
+import net.luckperms.api.event.user.UserDataRecalculateEvent;
+import net.luckperms.api.model.user.User;
 import org.slf4j.Logger;
 
+import java.time.Duration;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @com.velocitypowered.api.plugin.Plugin(
         id = "velocitab",
@@ -34,6 +36,7 @@ import java.util.UUID;
 public class Plugin {
     private final ProxyServer server;
     private final Logger logger;
+    private LuckPerms luckPerms;
     private NameFormatter nameFormatter;
 
     @Inject
@@ -44,52 +47,64 @@ public class Plugin {
 
     @Subscribe
     public void onProxyInitialize(ProxyInitializeEvent event) {
-        nameFormatter = new NameFormatter(LuckPermsProvider.get().getUserManager());
+        luckPerms = LuckPermsProvider.get();
+        nameFormatter = new NameFormatter(luckPerms.getUserManager());
 
-//        EventManager manager = server.getEventManager();
-//        manager.register(this, new JoinListener(util, server));
-
-//        server.getCommandManager().register("test", new TestCommand());
+        EventBus eventBus = luckPerms.getEventBus();
+        eventBus.subscribe(this, GroupDataRecalculateEvent.class, this::onLuckPermsGroupDataRecalculate);
+        eventBus.subscribe(this, UserDataRecalculateEvent.class, this::onLuckPermsUserDataRecalculate);
     }
 
+    public void onLuckPermsGroupDataRecalculate(GroupDataRecalculateEvent event) {
+        server.getAllPlayers()
+                .stream()
+                .filter(p -> p.hasPermission("group." + event.getGroup().getName()))
+                .forEach(this::updateTargetInAllLists);
+    }
+
+    public void onLuckPermsUserDataRecalculate(UserDataRecalculateEvent event) {
+        server.getPlayer(event.getUser().getUniqueId()).ifPresent(this::updateTargetInAllLists);
+    }
 
     @Subscribe
     public void onJoin(ServerPostConnectEvent event) {
-        Player target = event.getPlayer();
-//        String targetServerName = event.getServer().getServerInfo().getName();
-        String targetServerName = target.getCurrentServer().get().getServerInfo().getName();
-        modifyListPlayer(target, targetServerName);
+        server.getScheduler().buildTask(this, () -> {
+            Player target = event.getPlayer();
+            updateEntireList(target.getTabList());
+            updateTargetInAllLists(target);
+        }).delay(Duration.ofMillis(1000)).schedule();
     }
 
-//    @Subscribe
-//    public void onChat(PlayerChatEvent event) {
-//        Player target = event.getPlayer();
-//        String targetServerName = "blah";
-//        modifyListPlayer(target, targetServerName);
-//    }
+    private void updateEntireList(TabList tabList) {
+        server.getAllPlayers().forEach(onlinePlayer -> updateTargetInList(onlinePlayer, tabList));
+    }
 
-    private void modifyListPlayer(Player target, String targetServerName) {
-        server.getAllPlayers().forEach(p -> {
-            TabList tabList = p.getTabList();
-            tabList.addEntry(
-                    // gamemode should show properly to other people on the same server
-                    // since it modifies the existing entry
-                    tabList.removeEntry(
-                            target.getUniqueId()
-                    ).orElse(
-                            TabListEntry
-                                    .builder()
-                                    .tabList(tabList)
-//                                    .profile(target.getGameProfile())
-                                    .profile(GameProfile.forOfflinePlayer("placeholder"))
-                                    .playerKey(target.getIdentifiedKey())
-                                    .latency((int) target.getPing())
-                                    .gameMode(0) // default to survival since proxy doesn't know gamemode
-                                    .displayName(Component.text("stupid"))
-                                    .build()
-                    )
-//                    ).setDisplayName(nameFormatter.formatUsername(target, targetServerName))
-            );
-        });
+    private void updateTargetInAllLists(Player target) {
+        server.getAllPlayers().forEach(onlinePlayer -> updateTargetInList(target, onlinePlayer.getTabList()));
+    }
+
+    private void updateTargetInList(Player target, TabList tabList) {
+        tabList.removeEntry(
+                target.getUniqueId()
+        ).ifPresent(
+                e -> {
+                    Optional<ServerConnection> targetServer = target.getCurrentServer();
+                    targetServer.ifPresentOrElse(
+                            serverConnection -> tabList.addEntry(
+                                    e.setDisplayName(nameFormatter.formatUsername(
+                                            target,
+                                            serverConnection.getServerInfo().getName())
+                                    )
+                            ),
+                            () -> tabList.addEntry(
+                                    // TODO: replace this with a log warning
+                                    e.setDisplayName(nameFormatter.formatUsername(
+                                            target,
+                                            "null"
+                                    ))
+                            )
+                    );
+                }
+        );
     }
 }
