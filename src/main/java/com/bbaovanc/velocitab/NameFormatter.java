b36d194fde745ff8e349040594df41741b11ef81
BBaoVanC <bbaovanc@bbaovanc.com>
2022-08-07 21:12:05 -0500

    Almost almost done

--- src/main/java/com/bbaovanc/velocitab/NameFormatter.java
+++ src/main/java/com/bbaovanc/velocitab/NameFormatter.java
@@ -9,15 +9,37 @@ import net.luckperms.api.cacheddata.CachedMetaData;
 import net.luckperms.api.model.user.User;
 import net.luckperms.api.model.user.UserManager;
 
+import javax.annotation.Nullable;
 import java.util.Optional;
 
 public class NameFormatter {
     private final MiniMessage miniMessage;
     private final UserManager userManager;
 
-    public NameFormatter(UserManager userManager) {
+    public NameFormatter(UserManager userManager, MiniMessage miniMessage) {
         this.userManager = userManager;
-        this.miniMessage = MiniMessage.miniMessage();
+        this.miniMessage = miniMessage;
+    }
+
+    public TagResolver makePlaceholders(String username, String serverName, @Nullable CachedMetaData luckPermsMetaData, int onlineCount, long ping) {
+        TagResolver luckPermsResolver;
+        if (luckPermsMetaData != null) {
+            luckPermsResolver = TagResolver.builder()
+                    .resolver(Placeholder.parsed("luckperms_prefix",
+                            Optional.ofNullable(luckPermsMetaData.getPrefix()).orElse("")))
+                    .resolver(Placeholder.parsed("luckperms_suffix",
+                            Optional.ofNullable(luckPermsMetaData.getSuffix()).orElse("")))
+                    .build();
+        } else {
+            luckPermsResolver = TagResolver.empty();
+        }
+        return TagResolver.builder()
+                .resolver(Placeholder.unparsed("username", username))
+                .resolver(Placeholder.unparsed("server", serverName))
+                .resolver(Placeholder.unparsed("online_count", String.valueOf(onlineCount)))
+                .resolver(Placeholder.unparsed("ping", String.valueOf(ping)))
+                .resolver(luckPermsResolver)
+                .build();
     }
 
     public Component formatUsername(Player player, String serverName) {
@@ -27,15 +49,9 @@ public class NameFormatter {
         }
         CachedMetaData metaData = lpUser.getCachedData().getMetaData();
         return miniMessage.deserialize(
-                Config.template,
-                TagResolver.builder()
-                        .resolver(Placeholder.unparsed("username", player.getUsername()))
-                        .resolver(Placeholder.unparsed("server", serverName))
-                        .resolver(Placeholder.parsed("luckperms_prefix",
-                                Optional.ofNullable(metaData.getPrefix()).orElse("")))
-                        .resolver(Placeholder.parsed("luckperms_suffix",
-                                Optional.ofNullable(metaData.getSuffix()).orElse("")))
-                        .build()
+                Config.DISPLAY_NAME_FORMAT,
+                // TODO: fix onlineCount and ping
+                makePlaceholders(player.getUsername(), serverName, metaData, 0, 0)
         );
     }
 }
--- src/main/java/com/bbaovanc/velocitab/Plugin.java
+++ src/main/java/com/bbaovanc/velocitab/Plugin.java
@@ -2,6 +2,7 @@ package com.bbaovanc.velocitab;
 
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.DisconnectEvent;
 import com.velocitypowered.api.event.player.ServerPostConnectEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Dependency;
@@ -9,12 +10,17 @@ import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.ServerConnection;
 import com.velocitypowered.api.proxy.player.TabList;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.cacheddata.CachedMetaData;
 import net.luckperms.api.event.EventBus;
 import net.luckperms.api.event.group.GroupDataRecalculateEvent;
 import net.luckperms.api.event.user.UserDataRecalculateEvent;
 import net.luckperms.api.model.user.User;
+import net.luckperms.api.model.user.UserManager;
+import net.luckperms.api.node.types.InheritanceNode;
 import org.slf4j.Logger;
 
 import java.time.Duration;
@@ -36,29 +42,44 @@ import java.util.stream.Collectors;
 public class Plugin {
     private final ProxyServer server;
     private final Logger logger;
+    private final MiniMessage miniMessage;
     private LuckPerms luckPerms;
+    private UserManager lpUserManager;
     private NameFormatter nameFormatter;
 
     @Inject
     public Plugin(ProxyServer server, Logger logger) {
         this.server = server;
         this.logger = logger;
+        this.miniMessage = MiniMessage.miniMessage();
     }
 
     @Subscribe
     public void onProxyInitialize(ProxyInitializeEvent event) {
         luckPerms = LuckPermsProvider.get();
-        nameFormatter = new NameFormatter(luckPerms.getUserManager());
+        lpUserManager = luckPerms.getUserManager();
+        nameFormatter = new NameFormatter(lpUserManager, miniMessage);
 
         EventBus eventBus = luckPerms.getEventBus();
         eventBus.subscribe(this, GroupDataRecalculateEvent.class, this::onLuckPermsGroupDataRecalculate);
         eventBus.subscribe(this, UserDataRecalculateEvent.class, this::onLuckPermsUserDataRecalculate);
+
+        // TODO: Refresh header and footer every so often since ping may change
+        // can't run getAllPlayers() since no one is online when the initialization happens
+//        server.getScheduler().buildTask(this, () -> server.getAllPlayers().forEach(this::refreshHeaderFooter))
+//                .delay(Duration.ofSeconds(30)).repeat(30).schedule();
     }
 
     public void onLuckPermsGroupDataRecalculate(GroupDataRecalculateEvent event) {
         server.getAllPlayers()
                 .stream()
-                .filter(p -> p.hasPermission("group." + event.getGroup().getName()))
+                .filter(p -> {
+                    User user = lpUserManager.getUser(p.getUniqueId());
+                    if (user == null) {
+                        return false;
+                    }
+                    return user.getNodes().contains(InheritanceNode.builder().group(event.getGroup()).build());
+                })
                 .forEach(this::updateTargetInAllLists);
     }
 
@@ -72,7 +93,12 @@ public class Plugin {
             Player target = event.getPlayer();
             updateEntireList(target.getTabList());
             updateTargetInAllLists(target);
+            refreshHeaderFooter(target);
         }).delay(Duration.ofMillis(1000)).schedule();
+    }
+
+    public void onLeave(DisconnectEvent event) {
+        refreshHeaderFooter(event.getPlayer());
     }
 
     private void updateEntireList(TabList tabList) {
@@ -97,7 +123,7 @@ public class Plugin {
                                     )
                             ),
                             () -> tabList.addEntry(
-                                    // TODO: replace this with a log warning
+                                    // TODO: replace this with a log warning, this should be impossible
                                     e.setDisplayName(nameFormatter.formatUsername(
                                             target,
                                             "null"
@@ -105,6 +131,31 @@ public class Plugin {
                             )
                     );
                 }
+        );
+    }
+
+    private void refreshHeaderFooter(Player target) {
+        Optional<ServerConnection> serverConnection = target.getCurrentServer();
+        String serverName;
+        if (serverConnection.isPresent()) {
+            serverName = serverConnection.get().getServerInfo().getName();
+        } else {
+            // TODO: replace this with a log warning, this should be impossible
+            serverName = "null";
+        }
+
+        CachedMetaData cachedMetaData = null;
+        User user = lpUserManager.getUser(target.getUniqueId());
+        if (user != null) {
+            cachedMetaData = user.getCachedData().getMetaData();
+        }
+
+        TagResolver tagResolver = nameFormatter.makePlaceholders(
+                target.getUsername(), serverName, cachedMetaData, server.getPlayerCount(), target.getPing()
+        );
+        target.sendPlayerListHeaderAndFooter(
+                miniMessage.deserialize(Config.HEADER_FORMAT, tagResolver),
+                miniMessage.deserialize(Config.FOOTER_FORMAT, tagResolver)
         );
     }
 }
