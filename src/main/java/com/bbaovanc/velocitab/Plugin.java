c54348e192e4d45283d274f2bb2429ce2d9aa86a
BBaoVanC <bbaovanc@bbaovanc.com>
2022-08-09 20:06:14 -0500

    Bump to 1.0.1

--- src/main/java/com/bbaovanc/velocitab/Plugin.java
+++ src/main/java/com/bbaovanc/velocitab/Plugin.java
@@ -30,7 +30,7 @@ import java.util.Optional;
         authors = {"bbaovanc"},
         description = "Simple tab list plugin for Velocity",
         url = "https://github.com/BBaoVanC/velocitab",
-        version = "1.0",
+        version = "1.0.1",
         dependencies = {
                 @Dependency(id = "luckperms")
         }
